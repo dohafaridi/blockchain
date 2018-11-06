2ce8c4958d8bb9a3747cfbb3a7594c63519613f3
dohafaridi <dohafaridi@gmail.com>
2018-11-06 20:59:17 +0100

    Deploy

---
+++ build/precache-manifest.3dbe4371bea2b5f5f18f763228a45b4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5dda082fc5ff20dccbc7",
+    "url": "/static/js/main.5dda082f.chunk.js"
+  },
+  {
+    "revision": "1e8757b983e8d04310bc",
+    "url": "/static/js/1.1e8757b9.chunk.js"
+  },
+  {
+    "revision": "5dda082fc5ff20dccbc7",
+    "url": "/static/css/main.c9216714.chunk.css"
+  },
+  {
+    "revision": "8201a5da92f05761a44076a022050304",
+    "url": "/index.html"
+  }
+];
